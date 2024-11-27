e86424669d1c1fecc66db7fca4b8cf63472f5e85
zhanex3 <75746282+zhanex3@users.noreply.github.com>
2024-11-27 16:43:16 -0700

    Modified to use images in prototracer.
    Will create H and CPP of all .png files in the folder directory you point this at. Overwrites any c files already existing.

--- ImageConverter.py
+++ ImageConverter.py
@@ -1,22 +1,10 @@
 from PIL import Image
 import io
+import os
 
-inputFile = "Example Files\CoelaToot.png"
-outputFile = "Output\CoelaToot.h"
-pictureOutput = "Output\CoelaToot.png"
-name = "CoelaToot"
-numColors = 16 
-
-def GetData(className, file):
-    image = Image.open(file).convert("P", palette = Image.ADAPTIVE, colors = numColors)
-    image.save(pictureOutput)
+def GetHeader(className,image):
     w, h = image.size
-    
-    image.seek(0)
-    pal = image.getpalette()
-
-    data = "#pragma once\n\n"
-    data += "#include \"..\..\Materials\Image.h\"\n\n"
+    data = "#include \"Scene/Materials/Static/Image.h\"\n\n"
     data += "class " + className + " : public Image{\n"
     data += "private:\n"
     data += "\tstatic const uint8_t rgbMemory[];\n"
@@ -26,7 +14,16 @@ def GetData(className, file):
     data += "\t\tSetSize(size);\n"
     data += "\t\tSetPosition(offset);\n"
     data += "\t}\n};\n\n"
+    return data
 
+def GetCpp(className, image):
+    
+    w, h = image.size
+    
+    image.seek(0)
+    pal = image.getpalette()
+
+    data = "#include \"" + className + ".h\"\n\n"
     data += "const uint8_t " + className + "::rgbMemory[] PROGMEM = {"
     
     for i in range(h):
@@ -55,13 +52,42 @@ def GetData(className, file):
             data += "};\n"
         else:
             data += ","
-
     return data
 
+folder = "images"
 
-output = GetData(name, inputFile)
+directory = os.fsencode(folder)
 
-print(output)
-f = open(outputFile, "w")
-f.write(output)
-f.close()
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".png"):
+        # print(os.path.join(directory, filename))
+        inputFile = folder + "/" + filename
+        name = filename.split(".")[0]
+
+        #image settings
+        numColors = 255
+        width = 272
+        height = 92
+        left = 0
+        top = 0
+
+        image = Image.open(inputFile).convert("P", palette = Image.ADAPTIVE, colors = numColors)
+
+        #crop if needed
+        image = image.crop((left, top, width, height))
+
+        headerOutput = GetHeader(name,image)
+        cppOutput = GetCpp(name,image)
+
+        #print(headerOutput)
+        #print(cppOutput)
+        headerFile = open(folder + "/" + name +".h", "w")
+        headerFile.write(headerOutput)
+        headerFile.close()
+
+        cppFile = open(folder + "/" +name +".cpp", "w")
+        cppFile.write(cppOutput)
+        cppFile.close()
+    else:
+        continue
